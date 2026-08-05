19d088be376f3920cea2274caccca7f09e2e37a8
Patrick Leonardo Ruchinski <patrick.leonardo1277@gmail.com>
2026-08-04 21:12:30 -0300

    Adiciona Exerc4

---
+++ Agosto - 08/Exerc4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exerc4 {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+
+        System.out.println("Informe o numero da sua inscrição (0 para finalizar):");
+        int inscricao = leitor.nextInt();
+
+        while (inscricao != 0){
+            System.out.println("Informe a sua altura:");
+            double altura = leitor.nextDouble();
+        }
+    }
+}
