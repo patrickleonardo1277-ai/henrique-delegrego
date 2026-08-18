f5a44544c5b5f78706ac051457f8f08faaaaa7fd
Patrick Leonardo Ruchinski <patrick.leonardo1277@gmail.com>
2026-08-18 20:52:30 -0300

    Update Exerc4.java
    Atualizar exerc4

--- Agosto - 08/18.08.26/Exerc4.java
+++ Agosto - 08/18.08.26/Exerc4.java
@@ -2,48 +2,49 @@ import java.util.Scanner;
 
 public class Exerc4 {
 
-    public static void maior(int[] vetor){
-        int maior = vetor[0];
+    public static void maior(int vetor[]){
+        int maior = Integer.MIN_VALUE;
 
-        for (int i=1; i< vetor.length; i++){
+        for (int i=0; i< vetor.length; i++){
             if (vetor[i]>maior){
                 maior=vetor[i];
             }
         }
+        System.out.println("O maior valor é: " + maior);
 
     }
 
-    public static void menor(int [] vetor) {
-        int menor = vetor[0];
+    public static void menor(int vetor[]) {
+        int menor = Integer.MAX_VALUE;
 
-        for (int i=1; i< vetor.length; i++){
+        for (int i=0; i< vetor.length; i++){
             if (vetor[i]<menor){
                 menor=vetor[i];
             }
         }
+        System.out.println("O menor valor é: " + menor);
 
     }
-    public static void media(int[] vetor){
-        int soma = 0;
+    public static void media(int vetor[]){
+        double soma = 0;
 
         for(int i=0; i< vetor.length; i++){
             soma+=vetor[i];
         }
+
+        double media = soma / vetor.length;
+        System.out.println("A média dos valores é: " + media);
     }
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
 
-        int[] vetor = new int[5];
+        int vetor[] = {1, 2, 3, 4, 5};
 
-        for(int i=0; i< vetor.length; i++) {
-            System.out.println("Digite o " +(i+1)+ " ° número:");
-            vetor[i] = leitor.nextInt();
-        }
+        maior(vetor);
+        menor(vetor);
+        media(vetor);
 
-        System.out.println("Maior número:" + maior(vetor));
-        System.out.println("Menor número:" + menor(vetor));
-        System.out.println("Média: %.2f%n", media(vetor));
 
 
     }
