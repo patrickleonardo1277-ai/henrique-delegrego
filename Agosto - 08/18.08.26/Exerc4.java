cbdbea5e5067dc298ed395c14698af5aa6b7cb99
Patrick Leonardo Ruchinski <patrick.leonardo1277@gmail.com>
2026-08-18 19:49:38 -0300

    Update Exerc4.java
    Atualizar git exerc4

--- Agosto - 08/18.08.26/Exerc4.java
+++ Agosto - 08/18.08.26/Exerc4.java
@@ -1,12 +1,7 @@
 import java.util.Scanner;
 
 public class Exerc4 {
-    public static void main(String[] args) {
 
-        Scanner leitor = new Scanner(System.in);
-
-
-    }
     public static void maior(int[] vetor){
         int maior = vetor[0];
 
@@ -35,7 +30,21 @@ public class Exerc4 {
             soma+=vetor[i];
         }
     }
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int[] vetor = new int[5];
+
+        for(int i=0; i< vetor.length; i++) {
+            System.out.println("Digite o " +(i+1)+ " ° número:");
+            vetor[i] = leitor.nextInt();
+        }
+
+        System.out.println("Maior número:" + maior(vetor));
+        System.out.println("Menor número:" + menor(vetor));
+        System.out.println("Média: %.2f%n", media(vetor));
 
 
-
+    }
 }
